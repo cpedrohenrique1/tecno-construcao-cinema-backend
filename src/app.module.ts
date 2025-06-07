a7315e7c660c5d22e38d13c350c46b5e6a6786f5
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2025-06-07 21:06:19 +0000

    add: salas route

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FilmeModule } from './filme/filme.module';
 import { PrismaService } from './prisma/prisma.service';
+import { SalasModule } from './salas/salas.module';
 
 @Module({
-  imports: [FilmeModule],
+  imports: [FilmeModule, SalasModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
