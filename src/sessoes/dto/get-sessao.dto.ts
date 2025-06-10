93971bd60fe641567306ba9f07100c5ba3dd531a
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2025-06-10 19:32:50 +0000

    add: getSessaoDto

---
+++ src/sessoes/dto/get-sessao.dto.ts
@@ -0,0 +1,22 @@
+import { CreateFilmeDto } from "src/filme/dto/create-filme.dto";
+import { CreateSalaDto } from "src/salas/dto/create-sala.dto";
+
+export class GetSessaoDto {
+    id: number;
+    filme: CreateFilmeDto;
+    sala: CreateSalaDto;
+    dataHora: Date;
+    preco: number;
+    idioma: string;
+    formato: string;
+
+    constructor(id: number, filme: CreateFilmeDto, sala: CreateSalaDto, dataHora: Date, preco: number, idioma: string, formato: string) {
+        this.id = id;
+        this.filme = filme;
+        this.sala = sala;
+        this.dataHora = dataHora;
+        this.preco = preco;
+        this.idioma = idioma;
+        this.formato = formato;
+    }
+}
