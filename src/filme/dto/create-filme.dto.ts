997e02df6e4bc45cd1dba56e0c33d39471fb2b68
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2025-06-10 19:32:43 +0000

    refactor: added constructor to Dtos

--- src/filme/dto/create-filme.dto.ts
+++ src/filme/dto/create-filme.dto.ts
@@ -5,4 +5,20 @@ export class CreateFilmeDto {
   classificacao: string;
   duracao: number;
   dataEstreia: Date;
+
+  constructor(
+    titulo: string,
+    descricao: string,
+    genero: string,
+    classificacao: string,
+    duracao: number,
+    dataEstreia: Date
+  ) {
+    this.titulo = titulo;
+    this.descricao = descricao;
+    this.genero = genero;
+    this.classificacao = classificacao;
+    this.duracao = duracao;
+    this.dataEstreia = dataEstreia;
+  }
 }
--- src/salas/dto/create-sala.dto.ts
+++ src/salas/dto/create-sala.dto.ts
@@ -2,4 +2,10 @@ export class CreateSalaDto {
   nome: string;
   capacidade: number;
   tipo: string;
+
+  constructor(nome: string, capacidade: number, tipo: string) {
+    this.nome = nome;
+    this.capacidade = capacidade;
+    this.tipo = tipo;
+  }
 }
