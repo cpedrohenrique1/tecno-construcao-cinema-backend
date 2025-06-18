eeb05aacb0ab9d911e7f4392722a765fd35d8257
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2025-06-17 22:18:38 -0300

    feat: update Docker configuration and entrypoint script for improved service orchestration

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ WORKDIR /app
 COPY . /app
 RUN npm install
 EXPOSE 3000
+EXPOSE 5555
 ENTRYPOINT [ "sh", "./entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,16 @@
+#!/bin/sh
 until nc -z db 5432; do
   echo "Aguardando o banco de dados..."
   sleep 2
 done
 
-npx prisma migrate dev --name init && npm run start:dev
+npx prisma migrate dev --name init
+
+# Inicia o server backend em segundo plano
+npm run start:dev &
+
+# Em seguida, inicia o Prisma Studio em primeiro plano
+npx prisma studio
+
+# Opcional: aguarda a finalização de todos os processos em background
+wait
